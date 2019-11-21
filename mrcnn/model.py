c1036ee42735243e0ac0bd61a4abd3bc828f5863
nsutphen1 <36973520+nsutphen1@users.noreply.github.com>
2019-11-21 03:16:27 -0600

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -29,6 +29,7 @@ from mrcnn import utils
 from distutils.version import LooseVersion
 assert LooseVersion(tf.__version__) >= LooseVersion("1.3")
 assert LooseVersion(keras.__version__) >= LooseVersion('2.0.8')
+tf.executing_eagerly()
 
 
 ############################################################
